d9caf7fdfabc3aa29dc89e2574c8cf9e8b3b5f68
aycatutku <arslanaycatutku@gmail.com>
2025-01-17 14:24:42 +0300

    fonts and font configurations added

--- App.js
+++ App.js
@@ -2,9 +2,22 @@ import React from 'react';
 import BottomTabNavigator from './src/navigation/BottomTabNavigator';
 import {Provider} from 'react-redux';
 import {store} from './src/store/store';
-import { ThemeProvider } from './src/context/ThemeContext';
+import {ThemeProvider} from './src/context/ThemeContext';
+import {useFonts} from "expo-font";
 
 export default function App() {
+
+    const [fontsLoaded, fontError] = useFonts({
+        'Roboto-SemiBold': require('./assets/fonts/Roboto-SemiBold.ttf'),
+        'Roboto-Regular': require('./assets/fonts/Roboto-Regular.ttf'),
+        'Roboto-Medium': require('./assets/fonts/Roboto-Medium.ttf'),
+        'Roboto-Bold': require('./assets/fonts/Roboto-Bold.ttf'),
+    });
+
+    if (!fontsLoaded && !fontError) {
+        return null;
+    }
+
     return (
         <Provider store={store}>
             <ThemeProvider>
