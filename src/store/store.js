b84cdaaf8b9101ca16909a330dca85a948f354b7
aycatutku <arslanaycatutku@gmail.com>
2025-01-18 14:36:17 +0300

    ui slice added in store

--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { baseApi } from '../api/baseApi';
+import statusHandlerMiddleware from "../middleware/statusHandlerMiddleware";
+import uiReducer from './uiSlice';
 
 export const store = configureStore({
     reducer: {
         [baseApi.reducerPath]: baseApi.reducer,
+        ui:uiReducer,
     },
     middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware().concat(baseApi.middleware),
+        getDefaultMiddleware().concat(statusHandlerMiddleware,baseApi.middleware),
 });
 
 export default store;
