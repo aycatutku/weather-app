1fbc535a253f0dd3f0436a90e1c1e69274aec649
aycatutku <arslanaycatutku@gmail.com>
2025-01-18 14:35:48 +0300

    update theme colors

--- src/constants/themes.js
+++ src/constants/themes.js
@@ -4,13 +4,15 @@ export const themes = {
         primaryColor: '#E8F9FF',
         secondaryColor: '#C4D9FF',
         elementColor: '#C5BAFF',
-        placeholderColor:'#999'
+        placeholderColor:'#999',
+        red:'#d92419'
     },
     night: {
         backgroundColor: '#000957',
         primaryColor:'#344CB7',
         secondaryColor:'#577BC1',
         elementColor: '#FFEB00',
-        placeholderColor:'rgba(204,204,184,0.76)'
+        placeholderColor:'rgba(204,204,184,0.76)',
+        red:'#ff0000'
     },
 };
