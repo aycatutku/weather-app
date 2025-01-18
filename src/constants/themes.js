ee4a41b9bb7bb389b8a4fd1c596234cedb397340
aycatutku <arslanaycatutku@gmail.com>
2025-01-18 13:27:04 +0300

    placeholder color added

--- src/constants/themes.js
+++ src/constants/themes.js
@@ -4,11 +4,13 @@ export const themes = {
         primaryColor: '#E8F9FF',
         secondaryColor: '#C4D9FF',
         elementColor: '#C5BAFF',
+        placeholderColor:'#999'
     },
     night: {
         backgroundColor: '#000957',
         primaryColor:'#344CB7',
         secondaryColor:'#577BC1',
         elementColor: '#FFEB00',
+        placeholderColor:'rgba(204,204,184,0.76)'
     },
 };
